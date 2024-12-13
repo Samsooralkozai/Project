ae598d6ce69a7498532af05b880cd3f21eb61a46
samsooralkozai <samsoor1alkozai@yahoo.com>
2024-12-13 12:20:32 -0500

    2nd update

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import plotly.express as px
+import pandas as pd
 
-st.write("Hello World")
+
+# load and clean the dataset
+df = pd.read_csv("./data/vehicles_us.csv")
+df_cleaned = df.dropna()
+
+# Plot the data
+fig = px.histogram(df, x='model_year', title='Distribution of Model Years')
+st.plotly_chart(fig)
